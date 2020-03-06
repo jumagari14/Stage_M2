a9b228edbb1adfb3817a0b573936036690309749
jumagari14 <jumagarilla@hotmail.com>
2020-03-06 14:51:32 +0100

    RNA-Seq pipeline in parallel

--- RNASeq_scripts/quant_rna_seq.sh
+++ RNASeq_scripts/quant_rna_seq.sh
@@ -36,68 +36,64 @@ echo "#############################"
 ## Load necessary modules
 module load jdk1.8/8u22
 module load python/3.7.2 
+# module load boost/1.69.0
+# module load samtools/1.9
+# module load htslib/1.9
+
+check=$(python3.7 -c "import HTSeq" | echo $?)
+if (($check==1))
+    then
+    pip3.7 install --user HTSeq 
+fi
 
 export LD_LIBRARY_PATH=/gpfs/softs/contrib/apps/gcc/7.3.0/lib64/:/gpfs/softs/contrib/apps/gcc/7.3.0/lib
 export LD_LIBRARY_PATH=/gpfs/softs/contrib/apps/python/3.7.2/bin/python3.7/lib/libpython3.7m.so:$LD_LIBRARY_PATH
-# list=$(ls subdata/*fq.gz | xargs -n 1 basename | sed 's/\(.*\)_.*/\1/' | sort -u)
 
-# # mkdir -p -m 755 trimm_data 
-# # mkdir -p -m 755 trimm_data/quality
+cd /gpfs/home/juagarcia/ 
+list=$(ls data/*fq | xargs -n 1 basename | sed 's/\(.*\)_.*/\1/' | sort -u)
 
-# gtf=$(find ./ -not -path '*/\R*' -name "*.gtf")
-# gff=$(find ./ -not -path '*/\R*' -name "*gff[3]*")
-# fasta=$(find ./ -name "*.fasta")
+mkdir -p -m 755 bin/trimm_data 
+mkdir -p -m 755 bin/trimm_data/quality
 
-# if [ ! -d "genome_ind" ] 
-#  then  ## Genome indexes are generated if necessary 
-#     mkdir -m 755 -p genome_ind 
-#     ./STAR \
-#     --runThreadN 16 \
-#     --runMode genomeGenerate \
-#     --genomeDir /gpfs/home/juagarcia/genome_ind \
-#     --genomeFastaFiles "$fasta" \
-#     --sjdbOverhang 149 \
-#     --sjdbGTFfile "$gtf" \
-#     --genomeChrBinNbits 12 
-# fi  
+#gtf=$(find data/ -not -path '*/\R*' -name "*.gtf")
+gff=$(find data/ -not -path '*/\R*' -name "*gff*")
+fasta=$(find data/ -name "*.fa")
 
-# mkdir -p -m 755 STAR_Align  
-# mkdir -p -m 755 counts
-# for I in $list
-# do
- 
-#     # cp subdata/"$I"_1.fq.gz subdata/"$I"_1.fastqsanger 
-#     # cp subdata/"$I"_2.fq.gz subdata/"$I"_2.fastqsanger
+if [ ! -d "bin/genome_ind" ] 
+ then  ## Genome indexes are generated if necessary 
+    mkdir -m 755 -p bin/genome_ind 
+    ./scripts/STAR \
+    --runThreadN 32 \
+    --runMode genomeGenerate \
+    --genomeDir ./bin/genome_ind \
+    --genomeFastaFiles "$fasta" \
+    --genomeSAindexNbases 12 \
+    --sjdbOverhang 149 \
+    --sjdbGTFfile "$gff" \
+    --genomeChrBinNbits 12 
+fi  
 
-#     #FastQC/fastqc subdata/"$I"_1.fq subdata/"$I"_2.fq --outdir=trimm_data/quality
+mkdir -p -m 755 bin/STAR_Align  
+mkdir -p -m 755 bin/counts
 
-#     # java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 128 -phred33 ./subdata/"$I"_1.fq.gz ./subdata/"$I"_2.fq.gz trimm_data/"$I"_1.par.fq.gz "$I"_1.unp.fq.gz trimm_data/"$I"_2.par.fq.gz "$I"_2.unp.fq.gz ILLUMINACLIP:Trimmomatic-0.39/adapters/TruSeq2-PE.fa:2:30:10 LEADING:25 TRAILING:25 SLIDINGWINDOW:5:20 MINLEN:50
 
-#     # rm -rf "$I"_1.unp.fq.gz "$I"_2.unp.fq.gz
-#./scripts/STAR --genomeLoad LoadAndExit --genomeDir ./bin/genome_ind
-## Load genome just once to save RAM memory 
-#     ./STAR \ --runThreadN 128 --genomeLoad NoSharedMemory --readFilesIn  --outSAMtype BAM SortedByCoordinate --twopassMode None --quantMode - --outSAMstrandField intronMotif --outSAMattrIHstart 1 --outSAMattributes NH HI AS nM NM MD MC jM jI ch --outSAMprimaryFlag OneBestScore --outSAMmapqUnique 60 --outSAMunmapped Within --outFilterType Normal --outFilterMultimapScoreRange 1 --outFilterMultimapNmax 10 --outFilterMismatchNmax 10 --outFilterMismatchNoverLmax 0.3 --outFilterMismatchNoverReadLmax 1.0 --outFilterScoreMin 0 --outFilterScoreMinOverLread 0.66 --outFilterMatchNmin 0 --outFilterMatchNminOverLread 0.66 --outSAMmultNmax -1 --outSAMtlen 1 --outBAMsortingThreadN 10 --outBAMsortingBinsN 50 --limitBAMsortRAM 143360000000
-#     --runThreadN 128 \
-#     --runMode alignReads \
-#     --outFilterMatchNmin 16 \
-#     --readFilesCommand zcat \
-#     --outSAMtype BAM Unsorted SortedByCoordinate \
-#     --genomeDir /gpfs/home/juagarcia/genome_ind \
-#     --outFileNamePrefix STAR_Align/"$I" \
-#     --readFilesIn trimm_data/"$I"_1.par.fq.gz  trimm_data/"$I"_2.par.fq.gz
+# cp subdata/"$I"_R1.fq.gz subdata/"$I"_R1.fastqsanger 
+# cp subdata/"$I"_R2.fq.gz subdata/"$I"_R2.fastqsanger
 
-#     ## In featureCounts, paired-end reads must have -p option!!! 
-#     ./featureCounts \
-#     -p \
-#     -t exon \
-#     -g gene_id \
-#     -F GTF \
-#     -Q 32 \
-#     -T 16 \
-#     -a "$gtf" \
-#     -o /gpfs/home/juagarcia/counts/"$I" \
-#     STAR_Align/"$I"Aligned.out.bam 
-# done
+parallel -j 3 "./scripts/FastQC/fastqc ./data/{}_R1.fq ./data/{}_R2.fq --outdir=./bin/trimm_data/quality" ::: $list
+parallel -j 3 "java -jar ./scripts/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 32 -phred33 ./data/{}_R1.fq ./data/{}_R2.fq ./bin/trimm_data/{}_R1.par.fq {}_R1.unp.fq ./bin/trimm_data/{}_R2.par.fq {}_R2.unp.fq ILLUMINACLIP:./scripts/Trimmomatic-0.39/adapters/TruSeq2-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:25" ::: $list
+
+parallel -j 3 "rm -rf {}_R1.unp.fq {}_R2.unp.fq" ::: $list
+
+./scripts/STAR --genomeLoad LoadAndExit --genomeDir ./bin/genome_ind
+# Load genome just once to save RAM memory 
+parallel --compress -j 3 "./scripts/STAR --runThreadN 32 --genomeDir ./bin/genome_ind --outFileNamePrefix ./bin/STAR_Align/{} --runMode alignReads --genomeLoad LoadAndKeep --readFilesIn ./bin/trimm_data/{}_R1.par.fq  ./bin/trimm_data/{}_R2.par.fq  --outSAMtype BAM SortedByCoordinate --twopassMode None --quantMode - --outSAMstrandField intronMotif --outSAMattrIHstart 1 --outSAMattributes NH HI AS nM NM MD MC jM jI ch --outSAMprimaryFlag OneBestScore --outSAMmapqUnique 60 --outSAMunmapped Within --outFilterType Normal --outFilterMultimapScoreRange 1 --outFilterMultimapNmax 10 --outFilterMismatchNmax 10 --outFilterMismatchNoverLmax 0.3 --outFilterMismatchNoverReadLmax 1.0 --outFilterScoreMin 0 --outFilterScoreMinOverLread 0.66 --outFilterMatchNmin 0 --outFilterMatchNminOverLread 0.66 --outSAMmultNmax -1 --outSAMtlen 1 --outBAMsortingThreadN 10 --outBAMsortingBinsN 50 --limitBAMsortRAM 10000200000" ::: $list
+./scripts/STAR --genomeLoad Remove --genomeDir ./bin/genome_ind 
+
+
+parallel -j 3 "python3.7 ./scripts/htseq-count --stranded=no STAR_Align/{}Aligned.sortedByCoord.out.bam "$gff"" ::: $list
+
+#parallel -j 3 "cufflinks -G "$gff" STAR_Align/{}Aligned.sortedByCoord.out.bam" ::: $list
 
 #STAR
 
@@ -108,31 +104,31 @@ export LD_LIBRARY_PATH=/gpfs/softs/contrib/apps/python/3.7.2/bin/python3.7/lib/l
 
 ##python3.6 tpm_to_C.py -d "$1"/tpm/ -f "$2"
 
-cd "$1"; 
+# cd "$1"; 
 
-files=$(find Conc/ -type f | sort)
-files=$(readlink -f $files)
+# files=$(find Conc/ -type f | sort)
+# files=$(readlink -f $files)
 
-count=0
-touch Conc.csv
+# count=0
+# touch Conc.csv
 
-for J in $files 
-do 
-    count=$((count+1))
-    iden=$(echo "$J" | rev | cut -d '/' -f 2 | rev | cut -d '_' -f 1)
-    cat "$J" > temp 
-    sed "1d" temp > tempfile ; mv tempfile temp
-    sed "1 i\Gene,$iden" temp > tempfile ; mv tempfile temp 
-    if (($count == 1))
-    then 
-        cat temp > Conc.csv
-    else 
-        cut -d ',' -f 2 temp  > count_ind
-        paste -d ',' Conc.csv count_ind > temp2 && mv temp2 Conc.csv 
-        rm -f temp2 
-    fi 
-done 
+# for J in $files 
+# do 
+#     count=$((count+1))
+#     iden=$(echo "$J" | rev | cut -d '/' -f 2 | rev | cut -d '_' -f 1)
+#     cat "$J" > temp 
+#     sed "1d" temp > tempfile ; mv tempfile temp
+#     sed "1 i\Gene,$iden" temp > tempfile ; mv tempfile temp 
+#     if (($count == 1))
+#     then 
+#         cat temp > Conc.csv
+#     else 
+#         cut -d ',' -f 2 temp  > count_ind
+#         paste -d ',' Conc.csv count_ind > temp2 && mv temp2 Conc.csv 
+#         rm -f temp2 
+#     fi 
+# done 
 
-rm count_ind temp 
+# rm count_ind temp 
 
-exit 0 
+# exit 0 
