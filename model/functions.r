486a98febec25eace633e0a0daf382337fba7500
jumagari14 <jumagarilla@hotmail.com>
2020-05-07 20:40:57 +0200

    Begin model in R

---
+++ model/functions.r
@@ -0,0 +1,8 @@
+fitWeight<-function(x,y){ ## From original fit_testpoids.m 
+  
+  verhulst<-
+  return(list("error":err,"message":info))
+}
+
+
+## Call global variables: assign("name",value,...,envir= .GlobalEnv) JUST ONCE IN MAIN
