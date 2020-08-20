957570140e1b4dd7454e33d92c899a42d9856a32
Juan-Manuel Garcia-Illarramendi <juan-manuel.garcia-illarramendi@inrae.fr>
2020-08-20 10:46:33 +0200

    Improve TPM calculation by simplifying the script

--- RNASeq_scripts/fpkm.py
+++ RNASeq_scripts/fpkm.py
@@ -5,23 +5,18 @@ Convert FPKM to TPM
 Based on equations explained in https://arxiv.org/pdf/1104.3889.pdf
 
 """
+
+
 ## Functions 
 def getTPM(filepath, filename,folder,count_file): 
     data=pd.read_table(filepath)
     count_data=pd.read_table(count_file,header=None,sep="\t",names=['tracking_id','count'])
-    for i in range(data.shape[0]): 
-        start_pos=re.search('[A-Za-z|0-9]*:([0-9]*)-([0-9]*)',str(data.iloc[i]["locus"])).group(1)
-        end_pos=re.search('[A-Za-z|0-9]*:([0-9]*)-([0-9]*)',str(data.iloc[i]["locus"])).group(2)
-        longueur=int(end_pos)-int(start_pos)+1
-        count_data.loc[count_data["tracking_id"]==data.iloc[i]["tracking_id"],"Length"]=longueur
-        count_data.loc[count_data["tracking_id"]==data.iloc[i]["tracking_id"],"Norm"]=count_data.loc[count_data["tracking_id"]==data.iloc[i]["tracking_id"],"count"]/longueur
+    data["longueur"]=[int(re.search('[A-Za-z|0-9]*:([0-9]*)-([0-9]*)',str(x)).group(2)) - int(re.search('[A-Za-z|0-9]*:([0-9]*)-([0-9]*)',str(x)).group(1))+1 for x in data["locus"]]
+    count_data=pd.merge(data,count_data,on="tracking_id")
+    count_data["Norm"]=count_data["count"]/count_data["longueur"]
     sum_norm=count_data.sum()["Norm"]
-    TPM=[]
-    for i in range(count_data.shape[0]): 
-        tpm=(count_data.iloc[i]["Norm"]/sum_norm)*10**6
-        TPM.append(tpm)
-    TPM_data=pd.DataFrame(data={"tracking_id": count_data["tracking_id"], "TPM": TPM})
-    TPM_data.drop(TPM_data.tail(5).index,inplace=True)
+    count_data["TPM"]=(count_data["Norm"]/sum_norm)*10**6
+    TPM_data=pd.DataFrame(data={"tracking_id": count_data["tracking_id"], "TPM": count_data["TPM"]})
     header=filename.split(".")[0]
     path=filepath.rsplit("/",2)[1].replace("FPKM","/tpm")
     TPM_data.to_csv(folder+"/"+path+"/"+header+"_TPM.csv",mode="w",index=False)
@@ -36,12 +31,12 @@ import numpy as np
 import os 
 import re
 
-parser=argparse.ArgumentParser(prog='TPM calculator', description='This script extracts the TPM values from a tab-delimited file based on FPKM values') 
+# parser=argparse.ArgumentParser(prog='TPM calculator', description='This script extracts the TPM values from a tab-delimited file based on FPKM values') 
 
-parser.add_argument('-d', action='store', dest='pathdir', type=str, help='Path to directory where gene and isoform measurements are stored in one folder per each sample')
-args=parser.parse_args()
+# parser.add_argument('-d', action='store', dest='pathdir', type=str, help='Path to directory where gene and isoform measurements are stored in one folder per each sample')
+# args=parser.parse_args()
 
-dirpath=args.pathdir
+dirpath="Red5Kiwi/fpkm/"
 
 new_direc=dirpath.split("/")[0]+"/tpm"
 print(new_direc)
